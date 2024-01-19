3748ceee5f0dffea54e59f5ec41f80d62da4ca2a
Atebi <156945353+Atebi@users.noreply.github.com>
2024-01-19 17:02:03 +0100

    Add redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/genesis",
+
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        // permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
